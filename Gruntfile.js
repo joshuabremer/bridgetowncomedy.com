617f4d42bd034838373f4fafd9d0f63b6c68d19f
Josh Bremer <jbremer@sweetspotdiabetes.com>
2015-01-07 22:35:48 -0800

    Minified code

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
         mangle: false,
-        beautify: true,
+        beautify: false,
         drop_debugger: false,
         drop_console: false,
         //compress: true
