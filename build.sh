e7ce5fc172baa5e9f409885ec79c836d1bb17ff3
Josh Bremer <jbremer@sweetspotdiabetes.com>
2015-04-11 11:07:01 -0700

    Updated site

--- build.sh
+++ build.sh
@@ -1,37 +1,43 @@
 cd bridgetown-ember;
-# git checkout master;
+git checkout master;
 
-# if git pull --ff origin master ; then
-#     echo "."
-# else
-#     echo "You have local changes that need to be resolved..."
-#     exit 1
-# fi
+if git pull --ff origin master ; then
+    echo "."
+else
+    echo "You have local changes that need to be resolved..."
+    exit 1
+fi
 
-# echo "\n\n\n\n======================================"
-# echo "Refreshing Festival Data..."
-# echo "======================================\n\n"
+echo "\n\n\n\n======================================"
+echo "Refreshing Festival Data..."
+echo "======================================\n\n"
 
-# if node _node_scripts/build.js ; then
-#     echo "Command succeeded"
-# else
-#     echo "Node Build Failed"
-#     exit 1
-# fi
+if node _node_scripts/build.js ; then
+    echo "Command succeeded"
+else
+    echo "Node Build Failed"
+    exit 1
+fi
 
-# git add .;
-# git commit -am "Updated site"
+git add .;
+git commit -am "Updated site";
 
-# if git push origin master ; then
-#     echo "Command succeeded"
-# else
-#     echo "Git push failed"
-#     exit 1
-# fi
+echo "\n\n\n\n======================================"
+echo "Files changed:"
+echo "======================================"
+git diff-tree --no-commit-id --name-status -r HEAD;
+echo "======================================\n\n"
 
-# echo "\n\n\n\n======================================"
-# echo "Building Ember App..."
-# echo "======================================\n\n"
+if git push origin master ; then
+    echo "Command succeeded"
+else
+    echo "Git push failed"
+    exit 1
+fi
+
+echo "\n\n\n\n======================================"
+echo "Building Ember App..."
+echo "======================================\n\n"
 
 if npm install ; then
     echo "Command succeeded"
@@ -73,4 +79,4 @@ git diff-tree --no-commit-id --name-status -r HEAD;
 echo "======================================\n\n"
 
 
-# git push origin gh-pages;
+git push origin gh-pages;
