76f4c754d24febcdf52fffd6e0271749300505db
Josh Bremer <jbremer@sweetspotdiabetes.com>
2015-04-11 10:19:54 -0700

    Updated site

--- _node_scripts/build.js
+++ _node_scripts/build.js
@@ -1,10 +1,10 @@
-var ShowBuilder = require( "./build_shows");
-var buildEvents = require( "./build_events");
-var buildVenues = require( "./build_venues");
-var PerformerBuilder = require( "./build_performers");
-var festivalData = require( "./festival-data" );
+var ShowBuilder = require( "./../bridgetown-ember/_node_scripts/build_shows");
+var buildEvents = require( "./../bridgetown-ember/_node_scripts/build_events");
+var buildVenues = require( "./../bridgetown-ember/_node_scripts/build_venues");
+var PerformerBuilder = require( "./../bridgetown-ember/_node_scripts/build_performers");
+var festivalData = require( "./../bridgetown-ember/_node_scripts/festival-data" );
 var Q = require( "q" );
-var util = require( "./utilities" );
+var util = require( "./../bridgetown-ember/_node_scripts/utilities" );
 
 var a = new ShowBuilder();
 a.createStaticPages();
---
+++ build.sh
@@ -0,0 +1,60 @@
+cd bridgetown-ember;
+git checkout master;
+
+if git pull --ff origin master ; then
+    echo "."
+else
+    echo "You have local changes that need to be resolved..."
+    exit 1
+fi
+
+echo "\n\n\n\n======================================"
+echo "Refreshing Festival Data..."
+echo "======================================\n\n"
+
+if node _node_scripts/build.js ; then
+    echo "Command succeeded"
+else
+    echo "Node Build Failed"
+    exit 1
+fi
+
+git add .;
+git commit -am "Updated site"
+
+if git push origin master ; then
+    echo "Command succeeded"
+else
+    echo "Git push failed"
+    exit 1
+fi
+
+echo "\n\n\n\n======================================"
+echo "Building Ember App..."
+echo "======================================\n\n"
+
+npm install;
+bower install;
+
+if ember build --environment="production" ; then
+    echo "Command succeeded"
+else
+    echo "Ember build failed"
+    exit 1
+fi
+
+cd ./../;
+
+echo "\n\n\n\n======================================"
+echo "Syncing Images..."
+echo "======================================\n\n"
+rsync -rv --size-only --delete ./bridgetown-ember/dist/festival-json/ ./festival-json;
+rsync -rv --size-only --delete ./bridgetown-ember/dist/assets/ ./assets;
+rsync -rv --size-only --delete ./bridgetown-ember/dist/img/ ./img;
+echo "\n\n\n\n======================================"
+echo "Building Jekyll Pages..."
+echo "======================================\n\n"
+node _node_scripts/build.js;
+git add .;
+git commit -am "Updated site"
+# git push origin gh-pages;
